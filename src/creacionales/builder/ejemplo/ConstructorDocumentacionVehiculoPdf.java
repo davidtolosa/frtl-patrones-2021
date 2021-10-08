6320a6b5e4a639094ab6c0406736d8b6d49852b1
David Tolosa <davidtolosa5@gmail.com>
2021-10-07 21:42:50 -0300

    Ejemplo Builder

---
+++ src/creacionales/builder/ejemplo/ConstructorDocumentacionVehiculoPdf.java
@@ -0,0 +1,21 @@
+package creacionales.builder.ejemplo;
+
+public class ConstructorDocumentacionVehiculoPdf extends ConstructorDocumentacionVehiculo {
+
+	public ConstructorDocumentacionVehiculoPdf() {
+		documentacion = new DocumentacionPdf();
+	}
+
+	public void construyeSolicitudPedido(String nombreCliente) {
+		String documento;
+		documento = "<PDF>Solicitud de pedido Cliente: " + nombreCliente + "</PDF>";
+		documentacion.agregaDocumento(documento);
+	}
+
+	public void construyeSolicitudMatriculacion(String nombreSolicitante) {
+		String documento;
+		documento = "<PDF>Solicitud de matriculacion Solicitante: " + nombreSolicitante + "</PDF>";
+		documentacion.agregaDocumento(documento);
+	}
+
+}
---
+++ src/creacionales/builder/ejemplo/DocumentacionPdf.java
@@ -0,0 +1,15 @@
+package creacionales.builder.ejemplo;
+
+public class DocumentacionPdf extends Documentacion {
+
+	public void agregaDocumento(String documento) {
+		if (documento.startsWith("<PDF>"))
+			contenido.add(documento);
+	}
+
+	public void imprime() {
+		System.out.println("Documentacion PDF");
+		for (String s : contenido)
+			System.out.println(s);
+	}
+}
