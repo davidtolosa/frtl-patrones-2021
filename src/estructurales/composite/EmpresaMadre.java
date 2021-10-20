beeacac3fe3bd84cb602992989911fd4ef20b8db
David Tolosa <davidtolosa5@gmail.com>
2021-10-19 21:36:06 -0300

    Composite Ejemplo

---
+++ src/estructurales/composite/EmpresaMadre.java
@@ -0,0 +1,24 @@
+package estructurales.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaMadre extends Empresa {
+	
+	protected List<Empresa> filiales = new ArrayList<Empresa>();
+
+	@Override
+	public double calculaCosteMantenimiento() {
+		double costoFiliales = 0.0;
+		for(Empresa filial: filiales)
+			costoFiliales = costoFiliales + filial.calculaCosteMantenimiento();
+		
+		return costoFiliales + (nVehiculos*costeUnitarioVehiculo);
+	}
+
+	@Override
+	public boolean agregaFilial(Empresa filial) {
+		return filiales.add(filial);
+	}
+
+}
---
+++ src/estructurales/composite/Main.java
@@ -0,0 +1,30 @@
+package estructurales.composite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Empresa empresa1 = new EmpresaSinFilial();
+		empresa1.agregaVehiculo();
+		empresa1.agregaVehiculo();
+		//System.out.println("Costo empresa 1: " + empresa1.calculaCosteMantenimiento());
+		
+		Empresa empresa2 = new EmpresaSinFilial();
+		empresa2.agregaVehiculo();
+		//System.out.println("Costo empresa 2: " + empresa2.calculaCosteMantenimiento());
+
+		Empresa empresa3 = new EmpresaMadre();
+		empresa3.agregaFilial(empresa1);
+		empresa3.agregaFilial(empresa2);
+		empresa3.agregaVehiculo();
+		//System.out.println("Costo empresa 3: " + empresa3.calculaCosteMantenimiento());
+		
+		Empresa empresa4 = new EmpresaMadre();
+		empresa4.agregaFilial(empresa3);
+		empresa4.agregaVehiculo();
+		empresa4.agregaVehiculo();
+		System.out.println("Costo empresa 4: " + empresa4.calculaCosteMantenimiento());
+		
+	}
+
+}
